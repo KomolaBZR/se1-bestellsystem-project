e569bbdebc277f9d4688ae2e438202438a3bef31
Komola Benzinger <s50544@bht-berlin.de>
2022-01-01 18:44:15 +0100

    feat.E2 completed

--- src/system/Printer.java
+++ src/system/Printer.java
@@ -1,5 +1,6 @@
 package system;
 
+import java.io.IOException;
 import java.util.List;
 
 import datamodel.Order;
@@ -32,9 +33,26 @@ public interface Printer {
 	public StringBuffer printOrder( Order order ); 
 	
 	/**
+	 * Print orders as table to a file.
+	 * 
+	 * Conditions:
+	 *  - creates new file or overwrites an existing file.
+	 *  - not existing parts of the path are creates, throws IOException
+	 *    if this is not possible.
+	 *    
+	 *    @param orders list of orders to print.
+	 *    @param filepath path and name of the output file.
+	 *    @throws IOException for errors.
+	 */
+	void printOrdersToFile( Iterable<Order> orders, String filepath ) throws IOException;
+	
+	
+	/**
 	 * Create new format
 	 * 
 	 * @return interpreter for printf-style format strings
 	 */
 	public Formatter createFormatter(); 
+	
+	
 }
