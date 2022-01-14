dc72790f441f2ca3d8c1f1100d156ccdadb6dbe2
Komola Benzinger <s50544@bht-berlin.de>
2022-01-14 19:59:23 +0100

    added Methods save() & printInventory in InventoryManagerImpl.java

--- src/system/impl/InventoryManagerImpl.java
+++ src/system/impl/InventoryManagerImpl.java
@@ -3,28 +3,34 @@ package system.impl;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import datamodel.Article;
+import datamodel.Currency;
 import datamodel.Order;
 import system.DataRepository;
+import system.Formatter;
 import system.InventoryManager;
 import system.RTE;
+import system.Formatter.TableFormatter;
 
 class InventoryManagerImpl implements InventoryManager {
 	/**
 	 * private static singleton instance.
 	 */
 	private static InventoryManager iM_instance = null;
-	
+
 	private final DataRepository.ArticleRepository articleRepository;
 
 	/**
 	 * internal data structure to manage inventory (unitsInStore) by Article-id's.
 	 */
-	private final Map<String,Integer> inventory;
+	private final Map<String, Integer> inventory;
 
 	/**
-	 * Static access method to InventoryManagerImpl singleton instance (singleton pattern).
+	 * Static access method to InventoryManagerImpl singleton instance (singleton
+	 * pattern).
 	 * 
 	 * @return reference to InventoryManagerImpl singleton instance.
 	 */
@@ -33,22 +39,20 @@ class InventoryManagerImpl implements InventoryManager {
 		/*
 		 * lazy instance creation (only when getInstance() is called)
 		 */
-		if( iM_instance == null ) {
+		if (iM_instance == null) {
 			iM_instance = new InventoryManagerImpl(articleRepository);
 		}
 		return iM_instance;
 	}
 
-
 	/*
 	 * Private constructor (singleton pattern).
 	 */
 
-	private InventoryManagerImpl(DataRepository.ArticleRepository articleRepository) { 
-		this.articleRepository=articleRepository;
-		this.inventory = new HashMap<>(); 
+	private InventoryManagerImpl(DataRepository.ArticleRepository articleRepository) {
+		this.articleRepository = articleRepository;
+		this.inventory = new HashMap<>();
 	}
-
 
 	@Override
 	public Optional<Article> findById(String id) {
@@ -56,13 +60,11 @@ class InventoryManagerImpl implements InventoryManager {
 		return null;
 	}
 
-
 	@Override
 	public Iterable<Article> findAll() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
 
 	@Override
 	public long count() {
@@ -70,13 +72,29 @@ class InventoryManagerImpl implements InventoryManager {
 		return 0;
 	}
 
-
+	/**
+	 * Create new article in InventoryManager (add to internal ArticleRepository).
+	 * 
+	 * @param article article to create.
+	 * @throws IllegalArgumentException if article is null, has no valid id or
+	 *                                  already exists.
+	 */
 	@Override
-	public Article save(Article entity) {
-		// TODO Auto-generated method stub
-		return null;
+	public Article save(Article article) {
+		if (article == null)
+			throw new IllegalArgumentException("illegal article: null");
+		//
+		String id = article.getId();
+		if (id == null)
+			throw new IllegalArgumentException("illegal article.id: null");
+		//
+		articleRepository.save(article); // save, make sure to avoid duplicates
+		//
+		if (!inventory.containsKey(id)) {
+			this.inventory.put(id, Integer.valueOf(0));
+		}
+		return article;
 	}
-
 
 	@Override
 	public int getUnitsInStock(String id) {
@@ -84,13 +102,11 @@ class InventoryManagerImpl implements InventoryManager {
 		return 0;
 	}
 
-
 	@Override
 	public void update(String id, int updatedUnitsInStock) {
 		// TODO Auto-generated method stub
-		
-	}
 
+	}
 
 	@Override
 	public boolean isFillable(Order order) {
@@ -98,20 +114,47 @@ class InventoryManagerImpl implements InventoryManager {
 		return false;
 	}
 
-
 	@Override
 	public boolean fill(Order order) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-
+	/**
+	 * Print inventory as table.
+	 * 
+	 * @return printed inventory (as table).
+	 */
 	@Override
 	public StringBuffer printInventory() {
-		// TODO Auto-generated method stub
-		return null;
+		return printInventory(StreamSupport.stream(articleRepository.findAll().spliterator(), false));
 	}
 
+	private StringBuffer printInventory(Stream<Article> articleStream) {
+		//
+		Formatter formatter = new FormatterImpl();
+		TableFormatter tfmt = new TableFormatterImpl(formatter, new Object[][] {
+				// five column table with column specs: width and alignment ('[' left, ']'
+				// right)
+				{ 12, '[' }, { 32, '[' }, { 12, ']' }, { 10, ']' }, { 14, ']' } }).liner("+-+-+-+-+-+") // print table
+																										// header
+						.hdr("||", "Inv.-Id", "Article / Unit", "Unit", "Units", "Value")
+						.hdr("||", "", "", "Price", "in-Stock", "(in €)").liner("+-+-+-+-+-+");
+		//
+		long totalValue = articleStream.map(a -> {
+			long unitsInStock = this.inventory.get(a.getId()).intValue();
+			long value = a.getUnitPrice() * unitsInStock;
+			tfmt.hdr("||", a.getId(), a.getDescription(),
+					formatter.fmtPrice(a.getUnitPrice(), a.getCurrency()).toString(), Long.toString(unitsInStock),
+					formatter.fmtPrice(value, a.getCurrency()).toString());
+			return value;
+		}).reduce(0L, (a, b) -> a + b);
+		//
+		String inventoryValue = formatter.fmtPrice(totalValue, Currency.EUR).toString();
+		tfmt.liner("+-+-+-+-+-+").hdr("", "", "Invent", "ory Value:", inventoryValue);
+		//
+		return tfmt.getFormatter().getBuffer();
+	}
 
 	@Override
 	public StringBuffer printInventory(int sortedBy, boolean decending, Integer... limit) {
